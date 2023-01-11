77297eb4f5d26fae49eda18f29af2f8ea1097cd7
jakapozun <j.pozun@nchain.com>
2023-01-11 15:58:19 +0100

    added sanity

---
+++ app/layout.tsx
@@ -0,0 +1,7 @@
+import {ReactNode} from "react";
+import '../styles/globals.css'
+export default function RootLayout({children}: { children: ReactNode }) {
+    return (<html>
+    <body>{children}</body>
+    </html>)
+}
---
+++ sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'xmexk9cd',
+    dataset: 'production'
+  }
+})
